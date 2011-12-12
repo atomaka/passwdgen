885829c2e454d02e3acf60000712eaf621643d84
Andrew Tomaka <atomaka@gmail.com>
2011-12-12 17:22:29 -0500

    Add more rules for passwords.

--- js/rules.js
+++ js/rules.js
@@ -4,3 +4,13 @@ special['53.com'] = (function(password) {
 	// 6-12 characters
 	return password.substr(0,12);
 });
+
+special['paypal.com'] = (function(password) {
+	// 8-20 characters
+	return password.substr(0,20);
+});
+
+special['ebay.com'] = (function(password) {
+	// 8-20 characters
+	return password.substr(0,20);
+});
